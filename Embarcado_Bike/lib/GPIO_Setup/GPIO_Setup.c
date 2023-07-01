d08307ce62ecc2b077b4d1300fbac937e8630f38
andrevale99 <andrefvale@gmail.com>
2023-07-01 20:46:16 -0300

    Configuracao do pino A1 como altenate function

--- Embarcado_Bike/lib/GPIO_Setup/GPIO_Setup.c
+++ Embarcado_Bike/lib/GPIO_Setup/GPIO_Setup.c
@@ -5,10 +5,12 @@
  * 
  * @note PC13 como output (LED)
  * @note PB12 como Input com PULL-UP
+ * @note PA1 como Altenate Function (TIM2_CH2)
 */
 void GPIO_setup(void)
 {
-    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOCEN | RCC_AHB1ENR_GPIOBEN;
+    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOCEN | RCC_AHB1ENR_GPIOBEN
+                    | RCC_AHB1ENR_GPIOAEN;
 
 
     //LED PC13
@@ -23,6 +25,11 @@ void GPIO_setup(void)
     GPIOB->OTYPER |= 0;
     GPIOB->OSPEEDR |= 0;
     GPIOB->PUPDR |= GPIO_PUPDR_PUPD12_0;
+
+    //PA1 TIM2_CH2
+    GPIOA->MODER |= GPIO_MODER_MODER1_1;
+    GPIOA->OSPEEDR |= GPIO_OSPEEDER_OSPEEDR1_1;
+    GPIOA->AFR[0] |= GPIO_AFRL_AFRL1_0;
 
 }
 
@@ -42,19 +49,4 @@ void EXTI_setup(void)
 
     NVIC_SetPriority(EXTI15_10_IRQn, 5);
     NVIC_EnableIRQ(EXTI15_10_IRQn);
-}
-
-/**
- * @brief Funcao de Interrupcao externa dos pinos
- * 10 ao 15
-*/
-void EXTI15_10_IRQHandler(void)
-{
-    //Verifica se foi o pino PB12 o acionado
-    if(EXTI->PR & EXTI_PR_PR12)
-    {
-        //Desativa o bit de espera
-        EXTI->PR |= EXTI_PR_PR12; 
-    }
-    //NVIC_ClearPendingIRQ(EXTI15_10_IRQn);
 }
