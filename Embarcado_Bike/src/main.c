886728e1cf6872e4c733ca116dc414e27291605a
andrevale99 <andrefvale@gmail.com>
2023-07-01 20:47:04 -0300

    Colocao da funcao interrupcao no arquivo MAIN

--- Embarcado_Bike/src/main.c
+++ Embarcado_Bike/src/main.c
@@ -1,9 +1,10 @@
 #include <stm32f411xe.h>
 #include "GPIO_Setup.h"
-
-extern volatile uint32_t flag;
+#include "TIMER_Setup.h"
 
 void Setup(void);
+
+extern void EXTI15_10_IRQHandler(void);
 
 int main(void)
 {
@@ -22,4 +23,22 @@ void Setup(void)
 {
     GPIO_setup();
     EXTI_setup();
+
+    TIMER_Setup();
+}
+
+/**
+ * @brief Funcao de Interrupcao externa dos pinos
+ * 10 ao 15
+*/
+void EXTI15_10_IRQHandler(void)
+{
+    //Verifica se foi o pino PB12 o acionado
+    if(EXTI->PR & EXTI_PR_PR12)
+    {
+        //Desativa o bit de espera
+        EXTI->PR |= EXTI_PR_PR12;
+        TIM2->CCER ^= TIM_CCER_CC2E;
+    }
+    //NVIC_ClearPendingIRQ(EXTI15_10_IRQn);
 }
