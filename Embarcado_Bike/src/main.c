e44e07450b28bffdd14f1f09136a72c6af41d3b8
andrevale99 <andrefvale@gmail.com>
2023-07-01 21:53:07 -0300

    Comentarios

--- Embarcado_Bike/src/main.c
+++ Embarcado_Bike/src/main.c
@@ -1,11 +1,20 @@
 #include <stm32f411xe.h>
 #include "GPIO_Setup.h"
 #include "TIMER_Setup.h"
+//=======================================================
+//  VARIAVEIS
+//=======================================================
 
-void Setup(void);
+//=======================================================
+//  PROTOTIPOS
+//=======================================================
+void Setup(void);                 
 
 extern void EXTI15_10_IRQHandler(void);
 
+//=======================================================
+//  MAIN
+//=======================================================
 int main(void)
 {
 
@@ -13,12 +22,16 @@ int main(void)
 
     while(1)
     {
-
     }
 
     return 0;
 }
-
+//=======================================================
+//  FUNCOES
+//=======================================================
+/**
+ * @brief Setup dos Perifericos
+*/
 void Setup(void)
 {
     GPIO_setup();
@@ -34,11 +47,12 @@ void Setup(void)
 void EXTI15_10_IRQHandler(void)
 {
     //Verifica se foi o pino PB12 o acionado
-    if(EXTI->PR & EXTI_PR_PR12)
+    if((EXTI->PR & EXTI_PR_PR12))
     {
         //Desativa o bit de espera
         EXTI->PR |= EXTI_PR_PR12;
         TIM2->CCER ^= TIM_CCER_CC2E;
+        TIM2->CR1 ^= TIM_CR1_CEN; 
     }
     //NVIC_ClearPendingIRQ(EXTI15_10_IRQn);
 }
