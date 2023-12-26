bb0bb90df950161359bbf001af7cb29fd4399a55
andrevale99 <andrefvale@gmail.com>
2023-12-26 11:10:52 -0300

    Comentarios e Funcoes para o ADC

--- EmbarcadoBike_AVR/main.c
+++ EmbarcadoBike_AVR/main.c
@@ -6,24 +6,45 @@
 #include <avr/interrupt.h>
 #include <util/delay.h>
 
-#include "LCD.c"
+#include "LCD.h"
+//======================================
+//  VARIAVEIS
+//======================================
 
+//======================================
+//  PROTOTIPOS
+//======================================
+void ADC_setup();
 
+ISR(ADC_vect);
+//======================================
+//  MAIN
+//======================================
 int main()
 {
   init_4bitsLCD();
-  
-  char *msg = "andre\0";
-  for( ; *msg!='\0'; msg++)
-  {
-    LCD_cmd(*msg, WRITE);
-    _delay_ms(250);
-  }
+  ADC_setup();
 
+  sei();
+ 
   while(1)
   {
 
   }
 
   return 0;
+}
+//======================================
+//  FUNCOES
+//======================================
+void ADC_setup()
+{
+  ADMUX = ((1<<REFS0));
+  ADCSRA = ((1<<ADEN) | 0x07 | (1<<ADIE));
+
+  DIDR0 = 0x3E; //Somente o A0 está ativo
+}
+
+ISR(ADC_vect)
+{
 }
