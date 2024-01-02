820600ad7d25330e53f30d10aeef123c1862b434
andrevale99 <andrefvale@gmail.com>
2024-01-02 17:32:35 -0300

    Criação de um buffer e Logica para auto tirgger do ADC (não completo)

--- EmbarcadoBike_AVR/main.c
+++ EmbarcadoBike_AVR/main.c
@@ -14,9 +14,10 @@
 //======================================
 //  VARIAVEIS
 //======================================
-char *battery = "BAT%:";
-char buffer[10];
-char *velocity = "m/s:";
+const char *battery = "BAT%:";
+const char *velocity = "m/s:";
+
+volatile char buffer[16];
 
 volatile uint8_t  vel = 0;
 volatile uint32_t  bat = 0;
@@ -29,6 +30,11 @@ void setup();
  * @brief Funcao para cnfiguracao do ADC
 */
 void ADC_setup();
+
+/**
+ * @brief Funcao para cnfiguracao do TIME0
+*/
+void TIMER0_setup();
 
 /**
  * @brief Interrupcao do ADC
@@ -51,14 +57,9 @@ int main()
 
   while(1)
   {
-    ADCSRA |= (1<<ADSC);
-
     writeLCD(battery, 5);
-
-    bat = (bat * 100) / 1023;
-    snprintf(buffer, 10, "%d", bat);
-    writeLCD(&buffer[0], 3);
-
+    snprintf(buffer, 16, "%d ", TCNT0);
+    writeLCD(&buffer[0], 4);
     LCD_cmd(RETURN_HOME, CMD);
   }
 
@@ -70,16 +71,28 @@ int main()
 void setup()
 {
   ADC_setup();
-  UCSR0B = 0x00;
+  TIMER1_setup();
+  UCSR0B = 0x00; //Desativa os RX r TX do MCU
   init_4bitsLCD();
 }
 
 void ADC_setup()
 {
   ADMUX = ((1<<REFS0));
-  ADCSRA = ((1<<ADEN) | 0x07 | (1<<ADIE));
+  ADCSRA = ((1<<ADIE) | (1<<ADATE)| (0x07 << ADPS0));
+  ADCSRB |= (1 << ADTS1) | (1 << ADTS0);
 
   DIDR0 = 0x3E; //Somente o A0 está ativo
+
+  ADCSRA |= (1<<ADEN);
+}
+
+void TIMER0_setup()
+{
+  TCCR0A = (1<<WGM01); //Pinos OC0x Desativados e na conf. CTC
+  TCCR0B |= (1<<CS02) | (1<<(1<<CS00)); //Prescale de 256
+
+  OCR0A = 156;
 }
 
 ISR(ADC_vect)
@@ -91,4 +104,9 @@ ISR(ADC_vect)
   adc_MSB = ADCH;
 
   bat = (adc_MSB << 8) | adc_LSB;
+
+  bat = (bat * 100) / 1023;
+  snprintf(buffer, 16, "%d ", bat);
+
+  TIFR0 |= (1<<OCF0A);
 }
