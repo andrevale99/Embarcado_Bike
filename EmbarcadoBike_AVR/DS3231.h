f544fb4516cbe9c2f44abfb47caf43b1d375f19c
andrevale99 <andrefvale@gmail.com>
2024-01-03 18:07:18 -0300

    Funções e comentários

--- EmbarcadoBike_AVR/DS3231.h
+++ EmbarcadoBike_AVR/DS3231.h
@@ -25,14 +25,45 @@
 #define SCL PC5
 #define SDA PC4
 
-void start_bit_i2c(uint8_t pullup)
-{
 
+/**
+ * @brief Inicializa o protocolo I2C
+ * 
+ * @param pullup -> Se ativa o pullup dos pinos SCL e SDA
+ *                  necessario para realizar a comunicacao
+ *                  I2C
+*/
+void init_i2c(uint8_t pullup = 0)
+{
+    if(pullup)
+    {
+        I2C_DDR  &= ~((1<<SCL) | (1<<SDA));
+        I2C_PORT |= (1<<SCL) | (1<<SDA);
+    }
+    else
+        I2C_DDR  &= ~((1<<SCL) | (1<<SDA));
+
+    //Ajuste da frequência de trabalho - SCL = F_CPU/(16+2.TWBR.Prescaler)
+    TWBR = 18;
+    TWSR |= 0x01;//prescaler = 4;
+    TWCR |= (1<<TWINT) | (1<<TWEN) | (1<<TWIE); //habilita o TWI com interrupcao
 }
 
-void stop_bit_i2c()
+/**
+ * @brief Star bit do I2C
+*/
+void i2c_start_bit()
 {
-
+    TWCR |= (1<<TWINT) | (1<<TWSTA) | (1<<TWEN);
 }
+
+/**
+ * @brief Stop bit do I2C
+*/
+void i2c_stop_bit()
+{
+    TWCR |= ((1<<TWINT) | (1<<TWSTO) | (1<<TWEN)) & ~(1<<TWSTA);
+}
+
 
 #endif
