d1e785b609a729b7234561589704c819b04ab736
andrevale99 <andrefvale@gmail.com>
2024-01-03 17:45:33 -0300

    Inclusão de bibliotecas e funções

--- EmbarcadoBike_AVR/DS3231.h
+++ EmbarcadoBike_AVR/DS3231.h
@@ -1,9 +1,32 @@
 #ifndef DS3231_H
 #define DS3231_H
 
+#ifndef F_CPU
+#define F_CPU 16000000UL
+#endif
+
+#include <stdint.h>
+#include <avr/io.h>
+#include <util/twi.h> 
+#include <avr/interrupt.h>
+
 /**
  * Biblioteca para realizar a comunicacao
  * com o RTC DS3231 usando I2C com interrupcao
 */
+
+#define ADDR_DS3231 0xD0
+#define WRITE 1
+#define READ 0
+
+void start_bit_i2c(uint8_t pullup)
+{
+
+}
+
+void stop_bit_i2c()
+{
+
+}
 
 #endif
