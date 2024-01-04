57cdef3f7f3aa4205ab851d878efa02db3370c96
andrevale99 <andrefvale@gmail.com>
2024-01-03 22:11:01 -0300

    Defines para pegas as horas e o dia (os mais importantes até agora)

--- EmbarcadoBike_AVR/DS3231.h
+++ EmbarcadoBike_AVR/DS3231.h
@@ -19,6 +19,14 @@
 #define WRITE 0
 #define READ 1
 
+#define SECONDS 0x00
+#define MINUTES 0x01
+#define HOURS 0x02
+#define DAY 0x03
+#define DATE 0x04
+#define MONTH 0x05
+#define YEAR 0x06
+
 #define I2C_DDR DDRC
 #define I2C_PORT PORTC
 
